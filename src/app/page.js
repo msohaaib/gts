165c65757b5ce492c97fab1ba634a18174327311
msohaaib <msohaib9685055@gmail.com>
2025-04-19 04:27:11 +0500

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,15 @@
+import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1></h1>
+      <h1>Welcome to GTS!</h1>
+      <p>This is a simple static website built with Next.js.</p>
+      <p>
+        Visit our <a href="/services">services</a> page or{" "}
+        <a href="/about">about</a>
+        page for more information.
+      </p>
+      <Image src="/logo.svg" alt="Logo" width={60} height={30} />
     </>
   );
 }
